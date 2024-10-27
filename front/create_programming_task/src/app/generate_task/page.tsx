ca7ea106e2ae5a189bc098b133265c9b41422e2d
reikoyuasa <yuasa.reiko.k5@s.mail.nagoya-u.ac.jp>
2024-10-27 13:06:17 +0900

    #45 generate_task page

--- front/create_programming_task/src/app/generate_task/page.tsx
+++ front/create_programming_task/src/app/generate_task/page.tsx
@@ -3,17 +3,18 @@
 import React, { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import {
-  SelectChangeEvent,
-  Container,
-  Box,
-  Typography,
-  Grid2,
-  FormControl,
-  InputLabel,
-  Select,
-  MenuItem,
-  TextField,
-  Button,
+    SelectChangeEvent,
+    Container,
+    Box,
+    Typography,
+    Grid2,
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem,
+    TextField,
+    Button,
+    Chip,
 } from "@mui/material";
 import TaskCard from "../_components/taskCard";
 import AiTaskCard from "../_components/aiTaskCard";
@@ -28,228 +29,210 @@ import { Language } from "@/types/language";
 import { getAccessToken } from "@/lib/actions";
 
 type FormValues = {
-  language: string;
-  technique: string;
+    language: string;
+    technique: string;
 };
 
 const Page: React.FC = () => {
-  const searchParams = useSearchParams();
-  const initialLanguage = searchParams.get("language");
-  const initialTechnique = searchParams.get("technique");
-  // タスク表示用
-  // TODO ここはAPIから取得するように変更する
-  const [tasks, setTasks] = useState<Task[]>(mockTasks);
-  const [aiTasks, setAiTasks] = useState<AiTask[]>([]);
-  const displayTaskNum = 3; // 直近3つのタスクを表示
+    const searchParams = useSearchParams();
+    const initialLanguage = searchParams.get("language");
+    const initialTechnique = searchParams.get("technique");
+    // タスク表示用
+    // TODO ここはAPIから取得するように変更する
+    const [tasks, setTasks] = useState<Task[]>(mockTasks);
+    const [aiTasks, setAiTasks] = useState<AiTask[]>([]);
+    const displayTaskNum = 3; // 直近3つのタスクを表示
 
-  const { control, handleSubmit } = useForm<FormValues>();
+    const { control, handleSubmit } = useForm<FormValues>();
 
-  // 言語選択用
-  const [language, setLanguage] = useState<Language>("Python3");
-  const handleChange = (event: SelectChangeEvent) => {
-    setLanguage(event.target.value as Language);
-  };
-
-  useEffect(() => {
-    const fetchAiTasks = async (language: Language, technique: string) => {
-      const accessToken = await fetchAccessToken();
-
-      const response = await fetch("http://localhost:8000/gemini", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${accessToken}`,
-        },
-        body: JSON.stringify({
-          language: initialLanguage,
-          technique: initialTechnique,
-        }),
-      });
-
-      const responseJson = await response.json();
-      if (response.ok) {
-        console.log(`response: ${JSON.stringify(responseJson)}`);
-        // レスポンスを元にAIタスクを作成
-        setAiTasks(responseJson.tasks);
-      } else {
-        console.error("error");
-        console.log(JSON.stringify(responseJson));
-      }
+    // 言語選択用
+    const [language, setLanguage] = useState<Language>("Python3");
+    const handleChange = (event: SelectChangeEvent) => {
+        setLanguage(event.target.value as Language);
     };
 
-    if (initialLanguage && initialTechnique) {
-      // geminiのAPIを叩いてAIタスクを取得
-      // http://localhost:8000/gemini (method: POST)
-      fetchAiTasks(initialLanguage as Language, initialTechnique);
-    }
-  }, []);
+    useEffect(() => {
+        const fetchAiTasks = async (language: Language, technique: string) => {
+        const accessToken = await fetchAccessToken();
 
-  const menuItems = LANGUAGES.map((language) => (
-    <MenuItem key={language} value={language}>
-      {language}
-    </MenuItem>
-  ));
+        const response = await fetch("http://localhost:8000/gemini", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+            },
+            body: JSON.stringify({
+            language: initialLanguage,
+            technique: initialTechnique,
+            }),
+        });
 
-  const onSubmit: SubmitHandler<FormValues> = async (data) => {
-    console.log(data);
-    // ここでgeminiのAPIを叩く
-    // http://localhost:8000/gemini (method: POST)
-    const accessToken = await fetchAccessToken();
+        const responseJson = await response.json();
+        if (response.ok) {
+            console.log(`response: ${JSON.stringify(responseJson)}`);
+            // レスポンスを元にAIタスクを作成
+            setAiTasks(responseJson.tasks);
+        } else {
+            console.error("error");
+            console.log(JSON.stringify(responseJson));
+        }
+        };
 
-    const response = await fetch("http://localhost:8000/gemini", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${accessToken}`,
-      },
-      body: JSON.stringify(data),
-    });
+        if (initialLanguage && initialTechnique) {
+            // geminiのAPIを叩いてAIタスクを取得
+            // http://localhost:8000/gemini (method: POST)
+            fetchAiTasks(initialLanguage as Language, initialTechnique);
+        }
+    }, []);
 
-    const responseJson = await response.json();
-    if (response.ok) {
-      console.log(`response: ${JSON.stringify(responseJson)}`);
-      // レスポンスを元にAIタスクを作成
-      setAiTasks(responseJson.tasks);
-    } else {
-      console.error("error");
-      console.log(JSON.stringify(responseJson));
-    }
-  };
+    const menuItems = LANGUAGES.map((language) => (
+        <MenuItem key={language} value={language}>
+            {language}
+        </MenuItem>
+    ));
 
-  const fetchAccessToken = async () => {
-    const accessToken = await getAccessToken();
-    return accessToken.value;
-  };
+    const onSubmit: SubmitHandler<FormValues> = async (data) => {
+        console.log(data);
+        // ここでgeminiのAPIを叩く
+        // http://localhost:8000/gemini (method: POST)
+        const accessToken = await fetchAccessToken();
 
-  return (
-    <Container>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          // alignItems: "center",
-          verticalAlign: "top",
-          // justifyContent: "center",
-        }}
-      >
-        {/* 画面左側 */}
+        const response = await fetch("http://localhost:8000/gemini", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${accessToken}`,
+            },
+            body: JSON.stringify(data),
+        });
+
+        const responseJson = await response.json();
+        if (response.ok) {
+            console.log(`response: ${JSON.stringify(responseJson)}`);
+            // レスポンスを元にAIタスクを作成
+            setAiTasks(responseJson.tasks);
+        } else {
+            console.error("error");
+            console.log(JSON.stringify(responseJson));
+        }
+    };
+
+    const fetchAccessToken = async () => {
+        const accessToken = await getAccessToken();
+        return accessToken.value;
+    };
+
+    return (
+        <Container>
         <Box
-          sx={{
-            width: "80%",
+            sx={{
             display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
+            flexDirection: "row",
+            // alignItems: "center",
+            verticalAlign: "top",
             // justifyContent: "center",
-          }}
-          component="form"
-          noValidate
-          onSubmit={handleSubmit(onSubmit)}
+            }}
         >
-          <Typography
+            {/* 画面左側 */}
+            <Box
             sx={{
-              m: 2,
+                width: "80%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                // justifyContent: "center",
             }}
-            variant="h4"
-            gutterBottom
-          >
-            タスク作成
-          </Typography>
-          {/* 学習したプログラミング言語の選択欄 */}
-          <FormControl
-            sx={{
-              m: 2,
-              width: "80%",
-            }}
-          >
-            <Controller
-              name="language"
-              control={control}
-              defaultValue={language}
-              render={({ field }) => (
-                <>
-                  <InputLabel id="select-language-label">学習言語</InputLabel>
-                  <Select
-                    {...field}
-                    labelId="select-language-label"
-                    id="select-language"
-                  >
-                    {menuItems}
-                  </Select>
-                </>
-              )}
-            />
-          </FormControl>
-          {/* 学習内容の入力欄 */}
-          <FormControl sx={{ m: 1, width: "80%" }}>
-            <Controller
-              name="technique"
-              control={control}
-              render={({ field }) => (
-                <TextField
-                  {...field}
-                  required
-                  id="outlined-required"
-                  label="学習内容"
-                  defaultValue=""
-                  sx={{
-                    width: "80%",
-                  }}
-                  multiline
-                  rows={3}
+            component="form"
+            noValidate
+            onSubmit={handleSubmit(onSubmit)}
+            >
+            <Typography
+                sx={{
+                m: 2,
+                }}
+                variant="h4"
+                gutterBottom
+            >
+                タスク作成
+            </Typography>
+            {/* 学習したプログラミング言語の選択欄 */}
+            <FormControl
+                sx={{
+                m: 2,
+                width: "80%",
+                }}
+            >
+                <Controller
+                name="language"
+                control={control}
+                defaultValue={language}
+                render={({ field }) => (
+                    <>
+                    <InputLabel id="select-language-label">学習言語</InputLabel>
+                    <Select
+                        {...field}
+                        labelId="select-language-label"
+                        id="select-language"
+                    >
+                        {menuItems}
+                    </Select>
+                    </>
+                )}
                 />
-              )}
-            />
-          </FormControl>
-          {/* タスク作成ボタン */}
-          <Button
-            variant="contained"
-            sx={{
-              width: "80%",
-              backgroundColor: BUTTON_COLOR,
-              m: 1,
-            }}
-            type="submit"
-          >
-            タスク作成
-          </Button>
-          {/* // <Box
-                    // key={task.title}
-                    // sx={{
-                    //   display: "flex",
-                    //   flexDirection: "column",
-                    //   alignItems: "center",
-                    //   justifyContent: "center",
-                    // }}
-                    // >
-                    //   <Typography>
-                    //     {task.difficulty}
-                    //   </Typography>
-                    //   <Typography
-                    //     sx={{
-                    //       m: 1,
-                    //     }}
-                    //     variant="h5"
-                    //     gutterBottom
-                    //   >
-                    //     {task.title}
-                    //   </Typography>
-                    //   <Typography
-                    //     sx={{
-                    //       m: 1,
-                    //     }}
-                    //     variant="body1"
-                    //     gutterBottom
-                    //     >
-                    //     {task.description}
-                    //   </Typography>
-                    //   <Typography
-                    //     sx={{
+            </FormControl>
+            {/* 学習内容の入力欄 */}
+            <FormControl sx={{ m: 1, width: "80%" }}>
+                <Controller
+                name="technique"
+                control={control}
+                render={({ field }) => (
+                    <TextField
+                    {...field}
+                    required
+                    id="outlined-required"
+                    label="学習内容"
+                    defaultValue=""
+                    sx={{
+                        width: "80%",
+                    }}
+                    multiline
+                    rows={3}
+                    />
+                )}
+                />
+            </FormControl>
+            {/* タスク作成ボタン */}
+            <Button
+                variant="contained"
+                sx={{
+                width: "80%",
+                backgroundColor: BUTTON_COLOR,
+                m: 1,
+                }}
+                type="submit"
+            >
+                タスク作成
+            </Button>
+            {/* // <Box
+                        // key={task.title}
+                        // sx={{
+                        //   display: "flex",
+                        //   flexDirection: "column",
+                        //   alignItems: "center",
+                        //   justifyContent: "center",
+                        // }}
+                        // >
+                        //   <Typography>
+                        //     {task.difficulty}
+                        //   </Typography>
+                        //   <Typography
+                        //     sx={{
                         //       m: 1,
                         //     }}
-                        //     variant="body1"
+                        //     variant="h5"
                         //     gutterBottom
-                        //     >
-                        //     {task.example}
+                        //   >
+                        //     {task.title}
                         //   </Typography>
                         //   <Typography
                         //     sx={{
@@ -258,50 +241,71 @@ const Page: React.FC = () => {
                         //     variant="body1"
                         //     gutterBottom
                         //     >
-                        //     {task.answer}
+                        //     {task.description}
                         //   </Typography>
-                        // </Box> */}
-        </Box>
-        {/* 画面右側 */}
-        <Box
-          sx={{
-            width: "120%",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            // justifyContent: "center",
-          }}
-          component="form"
-        >
-          <Typography
+                        //   <Typography
+                        //     sx={{
+                            //       m: 1,
+                            //     }}
+                            //     variant="body1"
+                            //     gutterBottom
+                            //     >
+                            //     {task.example}
+                            //   </Typography>
+                            //   <Typography
+                            //     sx={{
+                            //       m: 1,
+                            //     }}
+                            //     variant="body1"
+                            //     gutterBottom
+                            //     >
+                            //     {task.answer}
+                            //   </Typography>
+                            // </Box> */}
+            </Box>
+            {/* 画面右側 */}
+            <Box
             sx={{
-              m: 2,
+                width: "120%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                // justifyContent: "center",
             }}
-            variant="h4"
-            gutterBottom
-          >
-            おすすめタスク
-          </Typography>
-          {/* Geminiからの出力 */}
-          <Grid2
-            container
-            spacing={2}
-            columns={{ xs: 4, sm: 8, md: 12 }}
-            sx={{ marginTop: "20px", padding: "0 5%" }}
-          >
-            {aiTasks.length > 0 &&
-              aiTasks.map((task: AiTask) => {
-                return (
-                  <Grid2 key={task.title} size={{ xs: 4, sm: 8, md: 12 }}>
-                    <AiTaskCard task={task} />
-                  </Grid2>
-                );
-              })}
-          </Grid2>
+            component="form"
+            >
+            <Typography
+                sx={{
+                m: 2,
+                }}
+                variant="h4"
+                gutterBottom
+            >
+                おすすめタスク
+            </Typography>
+            {/* タスクを生成したときの条件 */}
+            <Chip label={initialLanguage} sx={{ marginLeft: "10px" }} />
+            
+            {/* Geminiからの出力 */}
+            <Grid2
+                container
+                spacing={2}
+                columns={{ xs: 4, sm: 8, md: 12 }}
+                sx={{ marginTop: "20px", padding: "0 5%" }}
+            >
+                {aiTasks.length > 0 &&
+                aiTasks.map((task: AiTask) => {
+                    return (
+                    <Grid2 key={task.title} size={{ xs: 4, sm: 8, md: 12 }}>
+                        <AiTaskCard task={task} />
+                    </Grid2>
+                    );
+                })}
+            </Grid2>
+            </Box>
         </Box>
-      </Box>
-    </Container>
-  );
+        </Container>
+    );
 };
 
 export default Page;
